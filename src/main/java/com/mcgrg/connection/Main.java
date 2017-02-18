8c260906a29f5e5c9b411f2450f9ab510b0da08b
McGrg <mcgrg@yandex.ru>
2017-02-18 21:26:42 +0300

    Added Usergroups entity, EntityResponse class - temporary, EntityRequest class using kevinsawicki http-request, Tokens<T>

--- src/main/java/com/mcgrg/connection/Main.java
+++ src/main/java/com/mcgrg/connection/Main.java
@@ -1,22 +1,22 @@
 package com.mcgrg.connection;
 
-import com.mcgrg.entity.ConstructionSites;
-import com.mcgrg.entity.Response;
+import com.google.gson.reflect.TypeToken;
+import com.mcgrg.entity.Materials;
 
-import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] a) {
-        try {
-//            Type itemListType = new TypeToken<List<ConstructionSites>>(){}.getType();
+//        try {
+//            Type itemListType = new TypeToken<List<ConstructionSites>>() {
+//            }.getType();
 //            URL url = new URL("http://127.0.0.1:8080/ServletDBMySql");
 //            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
 //            BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream()));
 //            String s = br.readLine();
-//            List<ConstructionSites> parsesites = new Gson().fromJson(s, itemListType );
-//            for (ConstructionSites conssite:parsesites){
+//            List<ConstructionSites> parsesites = new Gson().fromJson(s, itemListType);
+//            for (ConstructionSites conssite : parsesites) {
 //                System.out.println(conssite.toString());
-
 //            Type itemListType = new TypeToken<List<Usergroups>>() {
 //            }.getType();
 //            URL url = new URL("http://127.0.0.1:8080/ServletDBMySql");
@@ -25,13 +25,19 @@ public class Main {
 //            for (Usergroups usergroupr : parseusergroups) {
 //                System.out.println(usergroupr.toString());
 //            }
-            Response resp = new Response();
-            LinkedList<ConstructionSites> conslist = resp.getList();
-            for (ConstructionSites conssite : conslist) {
-                System.out.println(conssite.toString());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+//            EntityListResponce<Materials> resp = new EntityListResponce<>();
+//            LinkedList<Materials> conslist = resp.getList(new TypeToken<LinkedList<Materials>>() {
+//            }.getType());
+//            for (Materials conssite : conslist) {
+//                System.out.println(conssite.toString());
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        EntityRequest er = new EntityRequest();
+        List<Materials> materialslist = er.setRequest(new TypeToken<List<Materials>>(){}.getType());
+        for (Materials mat :  materialslist) {
+            System.out.println(mat.toString());
         }
     }
 }
